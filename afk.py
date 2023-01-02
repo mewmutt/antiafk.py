ea77c210143463f8be1e103a06f3aebe8997ac53
boodahdaloot <121647905+boodahdaloot@users.noreply.github.com>
2023-01-02 12:13:06 -0500

    Add files via upload

---
+++ afk.py
@@ -0,0 +1,63 @@
+from pynput import keyboard
+from threading import Thread
+from time import sleep
+import random
+import pyautogui as pyg
+import pydirectinput, threading, time, sys, os
+
+w = "w"
+a = "a"
+s = "s"
+d = "d"
+period = "."
+comma =  ","
+
+
+
+def press_key(key):
+    pydirectinput.press(key)
+
+def hold_key(key, s):
+    pydirectinput.keyDown(key)
+    time.sleep(s)
+    pydirectinput.keyUp(key)
+
+
+
+def on_press(key, abortKey='esc'):    
+    try:
+        k = key.char  # single-char keys
+    except:
+        k = key.name  # other keys    
+
+    print('pressed %s' % (k))
+    if k == abortKey:
+        print('end loop ...')
+        return False  # stop listener
+
+def loop_fun():
+    while True:
+        x = random.randint(0, 1920)
+        y = random.randint(0, 1080)
+        pyg.moveTo(x,y,1)
+        pyg.press('w')
+        print (pyg.position())
+        press_key("w")
+        sleep(1)
+
+
+
+
+        
+if __name__ == '__main__':
+    abortKey = 't'
+    listener = keyboard.Listener(on_press=on_press, abortKey=abortKey)
+    listener.start()  # start to listen on a separate thread
+
+    # start thread with loop
+    Thread(target=loop_fun, args=(), name='loop_fun', daemon=True).start()
+
+    listener.join() 
+time.sleep(2)
+threading.Thread(target=start()).start()
+
